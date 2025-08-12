011dfeb37dea16aaa200e254613ef35f20930b1d
v20200315 <v20200315@gmail.com>
2025-08-12 13:08:24 +0800

    feat, set up root layout and constants

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,10 @@
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <div className=".flex h-screen flex-col">
+      ROOT
+      <main className="flex-1 wrapper">{children}</main>
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,25 @@
-import type {Metadata} from "next";
-import {Inter} from "next/font/google";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "@/assets/styles/globals.css";
+import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
 
-const inter = Inter({subsets: ["latin"]});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Prostore",
-    description: "A modern e-commerce platform built with Next.js",
+  title: {
+    template: `%s | Prostore`,
+    default: APP_NAME,
+  },
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVER_URL),
 };
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
-}>) {
-    return (
-        <html lang="en">
-        <body
-            className={`${inter.className} antialiased`}
-        >
-        {children}
-        </body>
-        </html>
-    );
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
 }
