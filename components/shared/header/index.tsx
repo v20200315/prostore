c2fdcd969c2d35ef6248ed412edc1ab5d9c99c8b
v20200315 <v20200315@gmail.com>
2025-08-12 14:54:53 +0800

    feat: add theme mode toggle

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
+import ModeToggle from "./mode-toggle";
 
 const Header = () => {
   return (
@@ -23,13 +24,14 @@ const Header = () => {
           </Link>
         </div>
         <div className="space-x-2">
+          <ModeToggle />
           <Button asChild variant="ghost">
             <Link href="/cart">
               <ShoppingCartIcon />
               Cart
             </Link>
           </Button>
-          <Button asChild variant="ghost">
+          <Button asChild>
             <Link href="/sign-in">
               <UserIcon />
               Sign In
