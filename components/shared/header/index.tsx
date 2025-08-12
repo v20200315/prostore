ff47687a2d49571b4c77d2388b3ce42adfd4b9ff
v20200315 <v20200315@gmail.com>
2025-08-12 17:37:38 +0800

    feat: add responsive sheet menu

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,9 +1,7 @@
-import { ShoppingCartIcon, UserIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
-import ModeToggle from "./mode-toggle";
+import Menu from "./menu";
 
 const Header = () => {
   return (
@@ -23,21 +21,7 @@ const Header = () => {
             </span>
           </Link>
         </div>
-        <div className="space-x-2">
-          <ModeToggle />
-          <Button asChild variant="ghost">
-            <Link href="/cart">
-              <ShoppingCartIcon />
-              Cart
-            </Link>
-          </Button>
-          <Button asChild>
-            <Link href="/sign-in">
-              <UserIcon />
-              Sign In
-            </Link>
-          </Button>
-        </div>
+        <Menu />
       </div>
     </header>
   );
