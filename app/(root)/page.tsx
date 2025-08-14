b1bc7e82872deb1661edb1a1f07ed8f49dd04bb5
v20200315 <v20200315@gmail.com>
2025-08-14 17:40:47 +0800

    feat: load products from db and show them

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,11 @@
 import ProductList from "@/components/shared/product/product-list";
-import sampleData from "@/db/sample-data";
+import { getLatestProduct } from "@/lib/actions/product.action";
 
-const Homepage = () => {
+const Homepage = async () => {
+  const lastestProducts = await getLatestProduct();
   return (
     <>
-      <ProductList
-        data={sampleData.products}
-        title="Newest Arrivals"
-        limit={4}
-      />
+      <ProductList data={lastestProducts} title="Newest Arrivals" limit={4} />
     </>
   );
 };
---
+++ lib/actions/product.action.ts
@@ -0,0 +1,14 @@
+"use server";
+import { PrismaClient } from "@prisma/client";
+import { convertToPlainObject } from "../utils";
+import { LATEST_PRODUCTS_LIMIT } from "../constants";
+
+// Get latest products
+export async function getLatestProduct() {
+  const prisma = new PrismaClient();
+  const data = await prisma.product.findMany({
+    take: LATEST_PRODUCTS_LIMIT,
+    orderBy: { createdAt: "desc" },
+  });
+  return convertToPlainObject(data);
+}
